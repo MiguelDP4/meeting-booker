e7784e7e36daf484acaa0e4d0a56481d428f105c
Miguel <migueldp4@gmail.com>
2020-10-19 16:58:34 -0600

    style options panel

--- src/Components/Options.js
+++ src/Components/Options.js
@@ -16,32 +16,37 @@ const Options = (props) => {
   }
 
   return (
-    <div className="options-container">
-      <Link id="home" className={`options-link ${
-        activePage === "home" && "main-color-background"
-      }`}
-      to="/" 
-      onClick={changePage}>HOME</Link>
-      <Link id="conference_rooms" className={`options-link ${
-        activePage === "conference_rooms" && "main-color-background"
-      }`}
-      to="/conference_rooms"
-      onClick={changePage}>CONFERENCE ROOMS</Link>
-      <Link id="search_bookings" className={`options-link ${
-        activePage === "search_bookings" && "main-color-background"
-      }`}
-      to="/search_bookings"
-      onClick={changePage}>SEARCH BOOKINGS</Link>
-      <Link id="my_bookings" className={`options-link ${
-        activePage === "my_bookings" && "main-color-background"
-      }`}
-      to="/my_bookings"
-      onClick={changePage}>MY BOOKINGS</Link>
-      <Link id="log_out" className={`options-link ${
-        activePage === "log_out" && "main-color-background"
-      }`}
-      to="/"
-      onClick={logOutAndClear} >LOG OUT</Link>
+    <div className="sidebar">
+      <div className="options-logo">
+        <h1>FICTICIOUS ENTERPRISES</h1>
+      </div>
+      <div className="options-container">
+        <Link id="home" className={`options-link ${
+          activePage === "home" && "main-color-background"
+        }`}
+        to="/" 
+        onClick={changePage}>HOME</Link>
+        <Link id="conference_rooms" className={`options-link ${
+          activePage === "conference_rooms" && "main-color-background"
+        }`}
+        to="/conference_rooms"
+        onClick={changePage}>CONFERENCE ROOMS</Link>
+        <Link id="search_bookings" className={`options-link ${
+          activePage === "search_bookings" && "main-color-background"
+        }`}
+        to="/search_bookings"
+        onClick={changePage}>SEARCH BOOKINGS</Link>
+        <Link id="my_bookings" className={`options-link ${
+          activePage === "my_bookings" && "main-color-background"
+        }`}
+        to="/my_bookings"
+        onClick={changePage}>MY BOOKINGS</Link>
+        <Link id="log_out" className={`options-link ${
+          activePage === "log_out" && "main-color-background"
+        }`}
+        to="/"
+        onClick={logOutAndClear} >LOG OUT</Link>
+      </div>
     </div>
   );
 }
